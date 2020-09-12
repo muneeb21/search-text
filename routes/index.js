0df02e595808cf2bba647e6fac3f937b267cb515
Muneeb Adil <muneebadil07@gmail.com>
2020-09-12 14:11:18 +0530

    add routes

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('router loaded');
+
+const Controller=require('../controllers/controller')
+
+router.post('/insert',Controller.insert);
+router.get('/select',Controller.select);
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
+module.exports = router;
